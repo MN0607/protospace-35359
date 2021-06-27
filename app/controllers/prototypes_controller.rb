f94ee584f293f7951dc7adb77b1806df1497dc89
MN0607 <kobukuro.mun@gmail.com>
2021-06-27 14:45:21 +0900

    secondcommit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
   before_action :show_edit_update_params, only: [:show, :edit, :update]
 
